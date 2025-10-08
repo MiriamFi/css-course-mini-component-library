488e1eb2a5f4d267848e5b0bf2bc5aafd78d4a7d
MiriamFi <mfinjord@gmail.com>
2025-10-08 10:51:59 +0200

    ProgressBar første utkast

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,48 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import styled from "styled-components";
+import { COLORS } from "../../constants.js";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+const sizeStyles = {
+  small: {
+    "--height": "8px",
+    "--outerBorderRadius": "4px",
+    "--padding": 0,
+  },
+  medium: {
+    "--height": "12px",
+    "--outerBorderRadius": "4px",
+    "--padding": 0,
+  },
+  large: {
+    "--height": "24px",
+    "--outerBorderRadius": "8px",
+    "--padding": "4px",
+  },
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const sizes = sizeStyles[size];
+  return (
+    <Progress style={sizes} id="progressbar" max={100} value={value}></Progress>
+  );
 };
+
+const Progress = styled.progress`
+  appearance: none;
+  width: 370px;
+  height: var(--height);
+
+  ::-webkit-progress-bar {
+    background-color: ${COLORS.transparentGray15};
+    border-radius: var(--outerBorderRadius);
+    padding: var(--padding);
+    box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  }
+
+  ::-webkit-progress-value {
+    background-color: ${COLORS.primary};
+    border-radius: ${(props) => (props.value >= 99 ? " 4px" : "4px 0 0 4px")};
+  }
+`;
 
 export default ProgressBar;
